b1d3c20a638c0e7da1c283cebaffbb78559b9cdf
tmatteso <tomasmatteson@Tomass-MBP-2.ucsf.edu>
2024-03-28 16:04:55 -0700

    chunk chr1

--- DNA_dist_utils.py
+++ DNA_dist_utils.py
@@ -143,9 +143,9 @@ def epoch(model, rank, criterion,
         print("data", data) # should be str
         # collect data
         encoded_sequence = tokenizer.encode(data[0])
-        print("encoded_sequence", encoded_sequence)
+        print("encoded_sequence", encoded_sequence.ids)
         # feed it through the model forward
-        output = model.forward(encoded_sequence)
+        output = model.forward(encoded_sequence.ids)
         # compute the loss
         loss = criterion()
         # normalize loss to account for batch accumulation
