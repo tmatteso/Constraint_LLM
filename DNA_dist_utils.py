8014cfd5dd511dcaa5345d0b3a42149aaf62f38c
tmatteso <tomasmatteson@Tomass-MBP-2.ucsfmedicalcenter.org>
2024-03-29 13:12:36 -0700

    chunk chr1

--- DNA_dist_utils.py
+++ DNA_dist_utils.py
@@ -143,7 +143,7 @@ def epoch(model, rank, criterion,
         #print("data", data) # should be str
         # collect data
         
-        encoded_sequence = torch.tensor(tokenizer.encode(data[0]).ids, dtype=torch.long).to(rank)[:131072]
+        encoded_sequence = torch.tensor(tokenizer.encode(data[0]).ids, dtype=torch.long).to(rank)[:65536]
         encoded_sequence = encoded_sequence.unsqueeze(0)
         print("encoded_sequence", encoded_sequence.shape) # should be torch.Size([1, N])
         # it wants logits to be torch.Size([1, vocab_size]) for CE loss
