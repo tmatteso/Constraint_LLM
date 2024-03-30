4d73ded70294cecaabcff909afcf6cc08d09fba9
tmatteso <tomasmatteson@Tomass-MBP-2.ucsfmedicalcenter.org>
2024-03-29 22:26:43 -0700

    chunk chr1

--- DNA_dist_utils.py
+++ DNA_dist_utils.py
@@ -142,14 +142,16 @@ def epoch(model, rank, criterion,
     for batch_i, (data) in enumerate(train_loader):
         #print("data", data) # should be str
         # collect data
-        # fix layer number to 20, just for fun
-        # fsdp + activation checkpointing + mixed precision: model dim = 8192, 32768 tokens is max
-        # fsdp + mixed precision: model dim = 8192, 32768 tokens is max
-        # mixed precision: model dim = 8192, 32768 tokens is max
+        # fix layer number to 5
+        # fsdp + activation checkpointing + mixed precision: model dim = 8192, 65536 tokens is max
+        # fsdp + mixed precision: model dim = 8192,  tokens is max
+        # mixed precision: model dim = 8192,  tokens is max
+
+        # with same layer number, but half the model dimension
 
         # chinchilla estimate for 150B tokens is ~ 10B params, this is ~10% of Whole genome across 500 genomes
         # 7.5 B tokens is ~ 500M params, ~1% of whole genome across 250 genomes
-        encoded_sequence = torch.tensor(tokenizer.encode(data[0]).ids, dtype=torch.long).to(rank)[:65536]
+        encoded_sequence = torch.tensor(tokenizer.encode(data[0]).ids, dtype=torch.long).to(rank)[:131072]
         encoded_sequence = encoded_sequence.unsqueeze(0)
         print("encoded_sequence", encoded_sequence.shape) # should be torch.Size([1, N])
         # it wants logits to be torch.Size([1, vocab_size]) for CE loss
