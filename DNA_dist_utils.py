6a91d84954b540765ad1660b41e820ff5a28e5de
tmatteso <tomasmatteson@Tomass-MBP-2.ucsfmedicalcenter.org>
2024-03-29 21:12:37 -0700

    chunk chr1

--- DNA_dist_utils.py
+++ DNA_dist_utils.py
@@ -142,8 +142,11 @@ def epoch(model, rank, criterion,
     for batch_i, (data) in enumerate(train_loader):
         #print("data", data) # should be str
         # collect data
-        # 65536 tokens is max without activation checkpointing, mixed precision, fsdp
-        encoded_sequence = torch.tensor(tokenizer.encode(data[0]).ids, dtype=torch.long).to(rank)[:32768]
+
+
+        # with model dim = 8192, 32768 tokens is definitely doable
+        # model dim = 8192, 32768 tokens is max without activation checkpointing, mixed precision, fsdp
+        encoded_sequence = torch.tensor(tokenizer.encode(data[0]).ids, dtype=torch.long).to(rank)[:262144]
         encoded_sequence = encoded_sequence.unsqueeze(0)
         print("encoded_sequence", encoded_sequence.shape) # should be torch.Size([1, N])
         # it wants logits to be torch.Size([1, vocab_size]) for CE loss
