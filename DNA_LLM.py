3f438e8011438363ca72c56891f0fc739a379317
tmatteso <tomasmatteson@Tomass-MBP-2.ucsf.edu>
2024-04-17 14:59:09 -0700

    transcripts done

--- DNA_LLM.py
+++ DNA_LLM.py
@@ -81,7 +81,7 @@ class ConstraintBertModel(nn.Module):
     def __init__(self, tokenizer, encoder_layers = 10, 
                  embedding_dim = 1024, ffn_embedding_dim = 4096,  
                 #embedding_dim = 8192, ffn_embedding_dim = 24576,
-                 embedding_dim = 2048, ffn_embedding_dim = 6144,
+                 #embedding_dim = 2048, ffn_embedding_dim = 6144,
                  #head_num = 64,
                  head_num = 16, 
                  dropout=0.1):
